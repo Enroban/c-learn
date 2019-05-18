90221f38fc2a359bf5d533a27e000954a4a4990f
lishanggeng <lishanggeng@hikrosoft.com>
2019-05-18 22:29:08 +0800

    动态，静态链接

--- Learn/main.c
+++ Learn/main.c
@@ -12,10 +12,6 @@
 int main(int argc, const char * argv[]) {
 //     insert code here...
 //    printf("你好 Hello, World!\n");
-    
-    const char str[] = "这里全是中文";
-    printf("\n输出字符数:%d\n", printf(str));
-    puts(str);
 
     zifuchuan();
 }
